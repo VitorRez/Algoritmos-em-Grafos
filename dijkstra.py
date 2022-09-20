84e77f57c828a47caf61eba7ca0f070348a439a1
VitorRez <vitorez2002@gmail.com>
2022-09-20 13:40:29 -0300

    algoritmos de caminhamento em grafos

---
+++ dijkstra.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+from igraph import *
+
+g = Graph.Read_Ncol("exemplos_caminho.ncol", weights='if_present', directed=True)
+
+A   =   []
+d   =   []
+f   =   []
+rot =   []
+s   =   []
+
+for i in g.vs:
+        d.append(10000000000.0)
+        rot.append(0)
+        A.append(i.index)
+d[0] = 0.0
+
+j = 0
+while A:
+    vr = g.vs[j]
+    A.remove(vr.index)
+    f.append(vr.index)
+    s = set(g.neighbors(vr, mode='out')) & set(A)
+    for i in s:
+        p = min(d[vr.index] + g.es.select(_source=vr.index, _target=i)['weight'][0], d[i])
+        if p < d[i]:
+            d[i] = p
+            rot[i] = int(vr['name'])
+    j = j + 1
+
+print(d)
+print(rot)
---
+++ floyd-warshall.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+from igraph import *
+
+g = Graph.Read_Ncol("exemplos_caminho.ncol", weights='if_present', directed=True)
+
+rotas = []
+rx = [] #rota auxiliar
+D = []
+dx = [] #distancia auxiliar
+
+for i in g.vs():
+    for j in g.vs():
+        if i.index == j.index:
+            dx.append(0.0)
+            rx.append(0)
+        else:
+            if g.are_connected(i.index, j.index) == True:
+                dx.append(g.es.select(_source=i.index, _target=j.index)['weight'][0])
+                rx.append(int(j['name']))
+            else:
+                rx.append(0)
+                dx.append(10000000000.0)
+    D.append(dx)
+    rotas.append(rx)
+    rx = []
+    dx = []
+
+for k in g.vs():
+    for i in g.vs():
+        for j in g.vs():
+            if D[i.index][k.index] + D[k.index][j.index] < D[i.index][j.index]:
+                D[i.index][j.index] = D[i.index][k.index] + D[k.index][j.index]
+                rotas[i.index][j.index] = rotas[i.index][k.index]
+                
+
+print(D)
+print(rotas)
+print("marcelo")
